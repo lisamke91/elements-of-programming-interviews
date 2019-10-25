4087b63e7be06446e782e9ea5a537b5d870460fb
lisamke91 <lisa.m.ke91@gmail.com>
2019-10-25 16:00:51 -0700

    6.5 DeleteDuplicates implementation & test

--- arrays/src/main/java/DeleteDuplicates.java
+++ arrays/src/main/java/DeleteDuplicates.java
@@ -3,10 +3,31 @@ import java.util.List;
 public class DeleteDuplicates {
 
     /*
-    6.5
+     * 6.5 Write a program which takes as input a sorted array and updates it so that all
+     * duplicates have been removed and the remaining elements have been shifted left
+     * to fill the emptied indices. Return the number of valid elements. Many languages have library
+     * functions for performing this operation — you cannot use these functions.
+     *
+     * Hint:There is an 0(n) time and 0(1) space solution.
     */
 
-    public static int deleteDuplicates(List<Integer> A) {
-        return 0;
+    public static int deleteDuplicates(List<Integer> A)
+    {
+        int nextAvailSpace = 1, currentIndex = 1, prevValue = A.get(0);
+        int elements = A.size() < 1 ? 0 : 1;
+
+        while(currentIndex < A.size()) {
+            int currentValue = A.get(currentIndex);
+            A.set(currentIndex, null);
+            if(currentValue != prevValue) {
+                A.set(nextAvailSpace, currentValue);
+                prevValue = currentValue;
+                nextAvailSpace++;
+                elements++;
+            }
+            currentIndex++;
+        }
+
+        return elements;
     }
 }
