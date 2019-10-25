aa2f3d09fcd683cf8dd98c98f327a3ce355c42cc
lisamke91 <lisa.m.ke91@gmail.com>
2019-10-25 16:07:02 -0700

    6.6 BuyAndSellStockOnce implementation & test

--- arrays/src/main/java/BuySellStockOnce.java
+++ arrays/src/main/java/BuySellStockOnce.java
@@ -3,11 +3,24 @@ import java.util.List;
 public class BuySellStockOnce {
 
     /*
-       6.6
+     * 6.6 Write a program that takes an array denoting the daily stock price,
+     * and returns the maximum profit that could be made by buying and then
+     * selling one share of that stock.
+     *
+     * Hint: Identifying the minimum and maximum is not enough since the minimum
+     * may appear after the maximum height. Focus on valid differences.
     */
 
     public static int buySellStockOnce(List<Integer> A) {
-
-        return 0;
+        int maxProfit = 0;
+        if(A.size() > 1) {
+            int lowestValue = A.get(0);
+            for(int i = 0; i < A.size(); i++) {
+                int currentValue = A.get(i);
+                maxProfit = Math.max(maxProfit, currentValue - lowestValue);
+                lowestValue = Math.min(lowestValue, currentValue);
+            }
+        }
+        return maxProfit;
     }
 }
