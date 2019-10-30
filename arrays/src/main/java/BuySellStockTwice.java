da99b0518b1b9fc64968c0bbed94555cb2538201
lisamke91 <lisa.m.ke91@gmail.com>
2019-10-30 12:20:40 -0700

    6.7 BuyAndSellStockTwice implementation & test 6.8 Primes implementation & test

--- arrays/src/main/java/BuySellStockTwice.java
+++ arrays/src/main/java/BuySellStockTwice.java
@@ -1,14 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class BuySellStockTwice {
 
     /*
-    6.7
+     * 6.7 Write a program that computes the maximum profit that can be made by
+     * buying and selling a share at most twice. The second buy must be made on
+     * another date after the first sale.
+     *
+     * Hint:What do you need to know about the first i elements when processing
+     * the (i+l)th element?
     */
 
     public static int buySellStockTwice(List<Integer> A) {
+        List<Integer> singleSaleMaxProfits = new ArrayList<>();
+        int maxProfit = 0, lowestValue, highestValue;
 
-        return 0;
+        if(A.size() > 1) {
+            lowestValue = A.get(0);
+            // traverse once to calculate max from single sale
+            for (Integer currentValue : A) {
+                maxProfit = Math.max(maxProfit, currentValue - lowestValue);
+                lowestValue = Math.min(currentValue, lowestValue);
+                singleSaleMaxProfits.add(maxProfit);
+            }
+
+            // traverse again from end
+            highestValue = A.get(A.size()-1);
+            for(int i = A.size() - 1; i > 2; i--) {
+                int currentValue = A.get(i);
+                maxProfit = Math.max(singleSaleMaxProfits.get(i - 1) + highestValue - currentValue, maxProfit);
+                highestValue = Math.max(highestValue, currentValue);
+            }
+        }
+        return maxProfit;
     }
 
 }
--- arrays/src/main/java/EnumeratePrimes.java
+++ arrays/src/main/java/EnumeratePrimes.java
@@ -1,13 +1,39 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class EnumeratePrimes {
 
     /*
-    6.8
+     * 6.8 Write a program that takes an integer argument and returns all the primes between 1 and that
+     * integer. For example,if the input is 18, you should return(2,3,5,7,11,13,17).
+     *
+     * Hint:Exclude the multiples of primes.
     */
 
     public static List<Integer> enumeratePrimes(int n) {
-        return Arrays.asList(1);
+        List<Integer> primesList = new ArrayList<>();
+
+        boolean [] isNotPrime = new boolean[n + 1];
+
+        isNotPrime[0] = isNotPrime[1] = true;
+
+        for(int i = 0; i < n/2; i++) {
+            if(!isNotPrime[i]) {
+                int multiplier = i + i;
+                while(multiplier <= n) {
+                    isNotPrime[multiplier] = true;
+                    multiplier += i;
+                }
+            }
+        }
+
+        for(int j = 0; j < isNotPrime.length -1; j++) {
+            if(!isNotPrime[j]) {
+                primesList.add(j);
+            }
+        }
+
+        return primesList;
     }
 }
