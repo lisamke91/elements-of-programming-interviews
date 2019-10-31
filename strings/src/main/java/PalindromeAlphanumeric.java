b826356057c84538008fc58bd924a09759895adb
lisamke91 <lisa.m.ke91@gmail.com>
2019-10-31 14:52:01 -0700

    7.5 Palindrome Test implementation

--- strings/src/main/java/PalindromeAlphanumeric.java
+++ strings/src/main/java/PalindromeAlphanumeric.java
@@ -1,11 +1,32 @@
 public class PalindromeAlphanumeric {
 
     /*
-    7.5
+     * 7.5 Implement a function that takes as input a string s and returns true if s is a palindrome
     */
 
     public static boolean isPalindrome(String input) {
+        boolean isPalindrome = true;
 
-        return false;
+        String trimString = trimString(input.toUpperCase());
+
+        for(int i = 0; i < trimString.length() / 2; i++) {
+            if(trimString.charAt(i) != trimString.charAt(trimString.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        return isPalindrome;
+    }
+
+    private static String trimString(String input) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if(currentChar <= 'Z' && currentChar >= 'A') {
+                stringBuilder.append(currentChar);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
