e79c50da9cea233d8f4cf7ea0f634d34367b458f
lisamke91 <lisa.m.ke91@gmail.com>
2019-10-30 17:36:55 -0700

    7.3 Spreadsheet Column Encoding implementation

--- strings/src/main/java/SpreadsheetColumnEncoding.java
+++ strings/src/main/java/SpreadsheetColumnEncoding.java
@@ -1,12 +1,18 @@
 public class SpreadsheetColumnEncoding {
 
     /*
-    7.3
+     * 7.3 Implement a function that converts a spreadsheet column id to the corresponding integer,
+     * with "A" corresponding to 1.
     */
 
     public static int decodeSpreadsheetColumn(String code) {
+        int integerRep = 0;
 
-        return 0;
+        for(int i = 0; i < code.length(); i++) {
+            integerRep += (code.charAt(i) - 'A' + 1) * Math.pow(26, i);
+        }
+
+        return integerRep;
     }
 
 
