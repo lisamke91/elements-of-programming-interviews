b3bda332cbcad57676267f67404ba6aec5367161
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-01 12:58:13 -0700

    7.9 Roman to decimal implementation

--- strings/src/main/java/RomanToDecimal.java
+++ strings/src/main/java/RomanToDecimal.java
@@ -1,11 +1,44 @@
+import java.util.HashMap;
+
 public class RomanToDecimal {
 
     /*
     7.9
     */
 
-    public static int romanToDecimal(String roman) {
+    private static final HashMap<Character, Integer> romanNumerals;
 
-        return 0;
+    static{
+        romanNumerals = new HashMap<>();
+        romanNumerals.put('I', 1);
+        romanNumerals.put('V', 5);
+        romanNumerals.put('X', 10);
+        romanNumerals.put('L', 50);
+        romanNumerals.put('C', 100);
+        romanNumerals.put('D', 500);
+        romanNumerals.put('M', 1000);
+    }
+
+    public static int romanToDecimal(String roman) {
+        int decimalValue = 0;
+
+        for(int i = 0; i < roman.length(); i++) {
+            char currentLetter = roman.charAt(i++);
+            int count = 1;
+            while(i < roman.length() && roman.charAt(i) == currentLetter) {
+                count++;
+                i++;
+            }
+
+            if(i > roman.length() - 1
+                    || romanNumerals.get(currentLetter) > romanNumerals.get(roman.charAt(i))) {
+                decimalValue += count * romanNumerals.get(currentLetter);
+                i--;
+            } else {
+                decimalValue += romanNumerals.get(roman.charAt(i)) - count * romanNumerals.get(currentLetter);
+            }
+        }
+
+        return decimalValue;
     }
 }
