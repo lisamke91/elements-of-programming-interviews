082420cd46261e6553f90364661430adb99829b7
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-02 18:14:43 -0700

    8.2 Reverse Sublist implementation

--- linkedlists/src/main/java/ReverseSingleSublist.java
+++ linkedlists/src/main/java/ReverseSingleSublist.java
@@ -5,7 +5,38 @@ public class ReverseSingleSublist {
     */
 
     public static ListNode<Integer> reverseSublist(ListNode<Integer> input, int s, int f) {
+        ListNode<Integer> sNode = null, temp, prevNode = null, currNode = input, nextNode = null;
+        int index = 1;
 
-        return new ListNode<>(null);
+        while(index < s) {
+            if(index == s - 1) {
+                prevNode = currNode;
+            }
+            currNode = currNode.next;
+            index++;
+        }
+
+        sNode = currNode;
+
+        // reverse sublist
+        while(index <= f) {
+            nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+            index++;
+        }
+
+        // swap beg end of list pointer
+        if(s == 1) {
+            input = prevNode;
+        }
+
+        if(sNode.next != null) {
+            sNode.next.next = prevNode;
+        }
+        sNode.next = currNode;
+
+        return input;
     }
 }
