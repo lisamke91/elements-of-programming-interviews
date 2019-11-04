658460f29c3c73055e4a949c105d98135e7fec8a
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-04 14:08:19 -0800

    8.10 Even Odd Merge implementation

--- linkedlists/src/main/java/EvenOddMerge.java
+++ linkedlists/src/main/java/EvenOddMerge.java
@@ -5,7 +5,23 @@ public class EvenOddMerge {
     */
 
     public static ListNode<Integer> merge(ListNode<Integer> list) {
-        return new ListNode<Integer>(null);
+
+        if(list.next != null) {
+            ListNode<Integer> temp, even = list, odd = list.next;
+
+            while(odd != null && odd.next != null) {
+                temp = odd.next;
+                odd.next = odd.next.next;
+                temp.next = even.next;
+                even.next = temp;
+
+                // traverse
+                even = even.next;
+                odd = odd.next;
+            }
+        }
+
+        return list;
     }
 
 }
