0a55ffc59aae5d2c7f00153f3f2b880ce68bd46a
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-04 11:45:32 -0800

    8.7 Delete Kth Last Node implementation

--- linkedlists/src/main/java/DeleteKthLastNode.java
+++ linkedlists/src/main/java/DeleteKthLastNode.java
@@ -1,11 +1,40 @@
 public class DeleteKthLastNode {
 
     /*
-    8.7
+     * 8.7 Without knowing the length of a linked list, it is not trivial to delete the kth last
+     * element in a singly linked list.
+     *
+     * Given a singly linked list and an integer k, write a program
+     * to remove the kth last element from the list. Your algorithm cannot use more than a few words
+     * of storage, regardless of the length of the list. In particular,you cannot assume that it is
+     * possible to record the length of the list.
     */
 
     public static void deleteNode(ListNode<Integer> list, int k) {
+        ListNode<Integer> endNode = list, nodeToDelete = list;
 
+        int index = 0;
+        while(index++ < k && endNode != null) {
+            endNode = endNode.next;
+        }
+
+        if(endNode != null) {
+            if(endNode.next == null) {
+                list = nodeToDelete;
+            }
+
+            while(endNode != null) {
+                endNode = endNode.next;
+                if(endNode == null && k == 1) {
+                    nodeToDelete.next = null;
+                }
+                nodeToDelete = nodeToDelete.next;
+            }
+
+            if(nodeToDelete != null) {
+                nodeToDelete.data = nodeToDelete.next.data;
+                nodeToDelete.next = nodeToDelete.next.next;
+            }
+        }
     }
-
 }
