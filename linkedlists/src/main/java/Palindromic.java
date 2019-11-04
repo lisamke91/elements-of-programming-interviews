6e9bae75bc081c109f35e5d68d41c8bdac3a5b99
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-04 14:37:12 -0800

    8.11 Palindromic singly linked list implementation

--- linkedlists/src/main/java/Palindromic.java
+++ linkedlists/src/main/java/Palindromic.java
@@ -5,8 +5,45 @@ public class Palindromic {
     */
 
     public static boolean isPalindromic(ListNode<Integer> list) {
+        boolean isPalindrome = true;
+        ListNode<Integer> currentNode = list;
 
-        return false;
+        int length = 0;
+        while(currentNode != null) {
+            length++;
+            currentNode = currentNode.next;
+        }
+        currentNode = list;
+
+        if(length > 1) {
+            ListNode<Integer> temp, prevNode = null, forward = null, backward = null;
+            int index = 0;
+            while(index < length / 2) {
+                temp = currentNode.next;
+                currentNode.next = prevNode;
+                prevNode = currentNode;
+                currentNode = temp;
+                index++;
+            }
+
+            backward = prevNode;
+            if(length % 2 == 0) {
+                forward = currentNode;
+            } else {
+                forward = currentNode.next;
+            }
+
+            while(forward != null) {
+                if(!forward.data.equals(backward.data)) {
+                    isPalindrome = false;
+                    break;
+                }
+                forward = forward.next;
+                backward = backward.next;
+            }
+        }
+
+        return isPalindrome;
     }
 
 }
