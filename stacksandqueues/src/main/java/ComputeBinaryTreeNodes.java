1de1db73287d3fea369b66468c308842f23e5ff4
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-05 14:09:43 -0800

    9.7 Compute binary tree nodes implementation

--- stacksandqueues/src/main/java/ComputeBinaryTreeNodes.java
+++ stacksandqueues/src/main/java/ComputeBinaryTreeNodes.java
@@ -1,4 +1,5 @@
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ComputeBinaryTreeNodes {
@@ -8,7 +9,38 @@ public class ComputeBinaryTreeNodes {
     */
 
     public static List<List<Integer>> binaryTreeDepthOrder(BinaryTree<Integer> tree) {
+        LinkedList<List<Integer>> depthsList = new LinkedList<>();
+        LinkedList<BinaryTree<Integer>> queue = new LinkedList<>();
+        queue.add(tree);
+        int leftAtCurrentDepth = 1, numNextDepth = 0, depth = 0;
 
-        return Collections.emptyList();
+        while(queue.size() > 0) {
+            leftAtCurrentDepth--;
+            BinaryTree<Integer> currentNode = queue.removeFirst();
+
+            if(depth > depthsList.size() - 1) {
+                depthsList.add(new LinkedList<>());
+            }
+            depthsList.get(depth).add(currentNode.data);
+
+            if(currentNode.left != null) {
+                queue.addLast(currentNode.left);
+                numNextDepth++;
+            }
+
+            if(currentNode.right != null) {
+                queue.addLast(currentNode.right);
+                numNextDepth++;
+            }
+
+            if(leftAtCurrentDepth == 0) {
+                leftAtCurrentDepth = numNextDepth;
+                numNextDepth = 0;
+                depth++;
+            }
+        }
+
+
+        return depthsList;
     }
 }
