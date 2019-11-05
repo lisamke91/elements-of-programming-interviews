0418dc429253412124848ec4f7997ce395b0a532
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-05 10:56:45 -0800

    9.3 Well formed brackets

--- stacksandqueues/src/main/java/IsStringWellFormed.java
+++ stacksandqueues/src/main/java/IsStringWellFormed.java
@@ -1,11 +1,44 @@
+import java.util.LinkedList;
+
 public class IsStringWellFormed {
 
     /*
-    9.3
+     * 9.3 Writea program that tests if a string made up of the characters'(',')','[',
+     * and"}' is well-formed.
     */
 
     public static boolean isWellFormed(String s) {
+        String [] brackets = s.split("");
+        LinkedList<String> openBrackets = new LinkedList<>();
+        boolean isWellFormed = true;
+        int index = 0;
 
-        return false;
+        while(index < brackets.length && isWellFormed) {
+            if(isOpenBracket(brackets[index])) {
+                openBrackets.push(brackets[index]);
+            } else {
+                isWellFormed = isMatchingBracket(openBrackets.pop(), brackets[index]);
+            }
+            index++;
+        }
+
+        return isWellFormed && openBrackets.size() == 0;
+    }
+
+    private static boolean isOpenBracket(String s) {
+        return s.equals("{") || s.equals("[") || s.equals("(");
+    }
+
+    private static boolean isMatchingBracket(String openBracket, String closedBracket) {
+        switch (openBracket) {
+            case "{":
+                return closedBracket.equals("}");
+            case "[":
+                return closedBracket.equals("]");
+            case "(":
+                return closedBracket.equals(")");
+            default:
+                return false;
+        }
     }
 }
