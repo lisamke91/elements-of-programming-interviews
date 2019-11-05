1aeb1637c7d086909168b435214417d6b906f8ca
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-04 16:47:51 -0800

    9.1 Stack with max() API implementation

--- stacksandqueues/src/main/java/StackWithMax.java
+++ stacksandqueues/src/main/java/StackWithMax.java
@@ -1,22 +1,42 @@
+import java.util.LinkedList;
+
 public class StackWithMax {
 
     /*
     9.1
     */
 
-    public StackWithMax() {
+    private LinkedList<Integer> stack;
+    private LinkedList<Integer> maxList;
 
+    public StackWithMax() {
+        stack = new LinkedList<>();
+        maxList = new LinkedList<>();
     }
 
     public Integer max() {
+        if(maxList.size() > 0) {
+            return maxList.getFirst();
+        }
         return 0;
     }
 
     public Integer pop() {
-        return 0;
+        int poppedValue = 0;
+        if(stack.size() > 0) {
+            poppedValue = stack.getFirst();
+            stack.removeFirst();
+            maxList.removeFirst();
+        }
+        return poppedValue;
     }
 
     public void push(Integer x) {
-
+        stack.push(x);
+        if(maxList.size() > 0) {
+            maxList.push(Math.max(x, maxList.getFirst()));
+        } else {
+            maxList.push(x);
+        }
     }
 }
