a7dadf2a802b382c2304a7eb374241f824449184
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-05 15:26:00 -0800

    9.10 Queue with MAX API implementation

--- stacksandqueues/src/main/java/QueueWithMax.java
+++ stacksandqueues/src/main/java/QueueWithMax.java
@@ -1,21 +1,40 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class QueueWithMax {
 
     /*
     9.10
     */
 
-    public void enqueue(Integer x) {
+    private Deque<Integer> maxValues = new LinkedList<>();
+    private Deque<Integer> queue = new LinkedList<>();
 
+    public void enqueue(Integer x) {
+        queue.add(x);
+        if(maxValues.size() == 0 || x <= maxValues.getLast()) {
+            maxValues.addLast(x);
+        } else {
+            while(maxValues.size() > 0 && x > maxValues.getLast()) {
+                maxValues.removeLast();
+            }
+            maxValues.addLast(x);
+        }
     }
 
     public Integer dequeue() {
-
-        return 0;
+        Integer result = null;
+        if(maxValues.size() > 0) {
+            result = queue.removeFirst();
+            if(maxValues.getFirst() == result) {
+                maxValues.removeFirst();
+            }
+        }
+        return result;
     }
 
     public Integer max() {
-
-        return 0;
+        return maxValues.size() > 0 ? maxValues.getFirst() : null;
     }
 
 
