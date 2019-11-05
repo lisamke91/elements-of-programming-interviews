0266a645f7270741ca23fc1c126446b7e054229d
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-05 15:01:10 -0800

    9.8 Circular queue implementation

--- stacksandqueues/src/main/java/CircularQueue.java
+++ stacksandqueues/src/main/java/CircularQueue.java
@@ -4,7 +4,7 @@ public class CircularQueue {
     9.8
     */
 
-    private int head = 0, tail = 0, numQueueElements = 0;
+    private int head = 0, tail = -1, numQueueElements = 0;
     private static final int SCALE_FACTOR = 2;
     private Integer[] entries;
 
@@ -13,15 +13,35 @@ public class CircularQueue {
     }
 
     public void enqueue(Integer x) {
-
+        if(numQueueElements == entries.length) {
+            Integer [] newArray = new Integer[entries.length * SCALE_FACTOR];
+            int newIndex = 0;
+            int index = head;
+            while(newIndex < numQueueElements) {
+                newArray[newIndex++] = entries[index];
+                index = index == entries.length - 1 ? 0 : index + 1;
+            }
+            head = 0;
+            tail = numQueueElements - 1;
+            entries = newArray;
+        }
+        tail = tail == entries.length - 1 ? 0 : tail + 1;
+        entries[tail] = x;
+        numQueueElements++;
     }
 
     public Integer dequeue() {
-
-        return 0;
+        Integer element = null;
+        if(numQueueElements > 0 ) {
+            element = entries[head];
+            entries[head] = null;
+            head = head == entries.length - 1 ? 0 : head + 1;
+        }
+        return element;
     }
 
     public int size() {
-        return 0;
+        return numQueueElements;
     }
+
 }
