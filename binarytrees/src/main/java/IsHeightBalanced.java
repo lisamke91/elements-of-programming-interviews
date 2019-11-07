74150d57b87f526d2c857b3e3f75f3941834fc39
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-06 16:42:05 -0800

    10.1 Height balanced binary tree implementation

--- binarytrees/src/main/java/IsHeightBalanced.java
+++ binarytrees/src/main/java/IsHeightBalanced.java
@@ -5,7 +5,25 @@ public class IsHeightBalanced {
     */
 
     public static boolean isBalanced(BinaryTree<Integer> tree) {
+        int treeHeight = checkTreeHeights(tree);
+        return treeHeight > 0;
+    }
 
-        return false;
+    private static int checkTreeHeights(BinaryTree<Integer> node) {
+        if(node == null) {
+            return 0;
+        }
+        if(node.left == null && node.right == null) {
+            return 1;
+        }
+
+        int leftHeight = checkTreeHeights(node.left);
+        int rightHeight = checkTreeHeights(node.right);
+        if(leftHeight < 0 || rightHeight < 0
+                || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
     }
 }
