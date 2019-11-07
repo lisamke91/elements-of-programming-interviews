0df959062f73ebfd0ae74d8f9e0409e19fc3b9ff
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-06 17:36:05 -0800

    10.2 Is tree symmetric implementation

--- binarytrees/src/main/java/IsSymmetric.java
+++ binarytrees/src/main/java/IsSymmetric.java
@@ -5,8 +5,25 @@ public class IsSymmetric {
     */
 
     public static boolean isSymmetric(BinaryTree<Integer> tree) {
+        return tree != null && determineIfSymmetric(tree.left, tree.right);
+    }
 
-        return false;
+    private static boolean determineIfSymmetric(BinaryTree<Integer> leftSubtreeNode,
+                                                BinaryTree<Integer> rightSubtreeNode) {
+        // base case 1, both null
+        if(leftSubtreeNode == null && rightSubtreeNode == null) {
+            return true;
+        }
+
+        // base case 2, one node is null
+        if(leftSubtreeNode == null || rightSubtreeNode == null) {
+            return false;
+        }
+
+        // recursive case
+        return leftSubtreeNode.data.equals(rightSubtreeNode.data)
+                && determineIfSymmetric(leftSubtreeNode.left, rightSubtreeNode.right)
+                && determineIfSymmetric(leftSubtreeNode.right, rightSubtreeNode.left);
     }
 
 }
