ddacea96f99187d1a6c3021f8a9013aafa1219b8
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-07 15:54:10 -0800

    10.4 Compute LCA with Parent field implementation

--- binarytrees/src/main/java/ComputeLCAWithParent.java
+++ binarytrees/src/main/java/ComputeLCAWithParent.java
@@ -5,7 +5,43 @@ public class ComputeLCAWithParent {
     */
 
     public static BinaryTreeParent<Integer> LCA(BinaryTreeParent<Integer> node0, BinaryTreeParent<Integer> node1) {
+        int height0 = 0, height1 = 0;
+        BinaryTreeParent<Integer> currentNode = node0, LCA = null;
+        while(currentNode.parent != null) {
+            height0++;
+            currentNode = currentNode.parent;
+        }
 
-        return new BinaryTreeParent<>(0);
+        currentNode = node1;
+        while(currentNode.parent != null) {
+            height1++;
+            currentNode = currentNode.parent;
+        }
+
+        BinaryTreeParent<Integer> largerHeight, smallerHeight;
+        if(height0 > height1) {
+            largerHeight = node0;
+            smallerHeight = node1;
+        } else {
+            largerHeight = node1;
+            smallerHeight = node0;
+        }
+
+        int heightDiff = Math.abs(height0 - height1);
+        while(heightDiff-- > 0 && largerHeight != null) {
+            largerHeight = largerHeight.parent;
+        }
+
+        while(largerHeight != null && smallerHeight != null) {
+            if(largerHeight.data.equals(smallerHeight.data)) {
+                LCA = largerHeight;
+                largerHeight = null;
+            } else {
+                largerHeight = largerHeight.parent;
+                smallerHeight = smallerHeight.parent;
+            }
+        }
+
+        return LCA;
     }
 }
