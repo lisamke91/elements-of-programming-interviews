c5ff13b3d45fedff1dca49316da0d48c0b677b6f
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-08 12:28:50 -0800

    10.6 Path Weight with target implementation

--- binarytrees/src/main/java/FindRootToLeafSum.java
+++ binarytrees/src/main/java/FindRootToLeafSum.java
@@ -1,12 +1,25 @@
 public class FindRootToLeafSum {
 
     /*
-    10.6
+     * 10.6 Find root to path wit specified sum
     */
 
     public static boolean hasPathSum(BinaryTree<Integer> tree, int targetSum) {
-
-        return false;
+        return hasPathSumHelper(tree, 0, targetSum);
     }
 
+    private static boolean hasPathSumHelper(BinaryTree<Integer> currentNode, int pathWeight, int targetSum) {
+        // base case 1: null node
+        if(currentNode == null) {
+            return false;
+        }
+        pathWeight += currentNode.data;
+        // base case 2: leaf node
+        if(currentNode.left == null && currentNode.right == null) {
+            return pathWeight == targetSum;
+        }
+        // recursive case
+        return hasPathSumHelper(currentNode.left, pathWeight, targetSum)
+                || hasPathSumHelper(currentNode.right, pathWeight, targetSum);
+    }
 }
