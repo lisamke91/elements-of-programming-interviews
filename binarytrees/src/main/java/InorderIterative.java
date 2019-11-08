227d874b844eee8d68ad46973afc46d791d77a21
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-08 12:53:58 -0800

    10.7 Inorder traversal without recursion implementation

--- binarytrees/src/main/java/InorderIterative.java
+++ binarytrees/src/main/java/InorderIterative.java
@@ -1,4 +1,5 @@
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InorderIterative {
@@ -8,7 +9,36 @@ public class InorderIterative {
     */
 
     public static List<Integer> BSTInOrder(BinaryTree<Integer> tree) {
+        LinkedList<BinaryTree<Integer>> stack = new LinkedList<>();
+        LinkedList<Integer> inOrder = new LinkedList<>();;
+        stack.push(tree);
 
-        return Collections.emptyList();
+        BinaryTree<Integer> currentNode;
+        BinaryTree<Integer> leftNode;
+        while(stack.size() > 0) {
+            currentNode = stack.pop();
+            leftNode = currentNode.left;
+            // base case: both children null
+            if(currentNode.left == null & currentNode.right == null) {
+                inOrder.add(currentNode.data);
+            } else {
+                // push right child onto stack and disconnect
+                if (currentNode.right != null) {
+                    stack.push(currentNode.right);
+                    currentNode.right = null;
+                }
+
+                // if left child, disconnect and push current node on stack
+                if (leftNode != null) {
+                    currentNode.left = null;
+                    stack.push(currentNode);
+                    stack.push(leftNode);
+                } else {
+                    inOrder.add(currentNode.data);
+                }
+            }
+        }
+
+        return inOrder;
     }
 }
