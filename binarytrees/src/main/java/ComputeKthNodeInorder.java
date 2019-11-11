94235fe9ae5075e916b21dd4eb3e9c6c06dc0aa5
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-11 12:45:09 -0800

    10.9 In-order Kth Node implementation

--- binarytrees/src/main/java/ComputeKthNodeInorder.java
+++ binarytrees/src/main/java/ComputeKthNodeInorder.java
@@ -5,7 +5,32 @@ public class ComputeKthNodeInorder {
     */
 
     public static BinaryTree<Integer> findKthNodeBinaryTree(BinaryTree<Integer> tree, int k) {
+        if(tree == null || tree.data < k) {
+            return null;
+        }
+        return findKthNodeHelper(tree, 0, k);
+    }
 
-        return new BinaryTree<Integer>(0);
+    private static BinaryTree<Integer> findKthNodeHelper(BinaryTree<Integer> currentNode, int nodeCount, int k) {
+        // base case 1: null node
+        if(currentNode == null) {
+            return currentNode;
+        }
+        // calculate current node count
+        int currentCount = nodeCount + (currentNode.left != null ? currentNode.left.data : 0);
+
+        // base case 2: current count = k
+        if(currentCount == k) {
+            return currentNode;
+        }
+
+        // recursive case: determine whether to proceed left or right
+        if(currentCount > k) {
+            // traverse left subtree
+            return findKthNodeHelper(currentNode.left, nodeCount, k);
+        } else {
+            // traverse right subtree
+            return findKthNodeHelper(currentNode.right, currentCount + 1, k);
+        }
     }
 }
