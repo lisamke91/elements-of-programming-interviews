b237e3837a635cb357139770f6c59283b307ec29
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-11 13:05:59 -0800

    10.10 Compute Successor implementation

--- binarytrees/src/main/java/ComputeSuccessor.java
+++ binarytrees/src/main/java/ComputeSuccessor.java
@@ -5,7 +5,29 @@ public class ComputeSuccessor {
     */
 
     public static BinaryTreeParent<Integer> findSuccessor(BinaryTreeParent<Integer> node) {
+        BinaryTreeParent<Integer> currentNode = node;
 
-        return new BinaryTreeParent<Integer>(0);
+        if(currentNode.right != null) {
+            // case 1: has right subtree ->
+            currentNode = currentNode.right;
+            while(currentNode.left != null) {
+                currentNode = currentNode.left;
+            }
+        } else {
+            // case 1: has no right subtree
+            boolean foundNextNode = false;
+            while(currentNode != null && !foundNextNode) {
+                if(currentNode.parent == null) {
+                    currentNode = currentNode.parent;
+                } else {
+                    if(currentNode.parent.left != null && currentNode.parent.left.data.equals(currentNode.data)) {
+                        foundNextNode = true;
+                    }
+                    currentNode = currentNode.parent;
+                }
+            }
+        }
+
+        return currentNode;
     }
 }
