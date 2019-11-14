ed9efcb94cdf325852761f3f2c70290fe39e1681
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-13 16:48:45 -0800

    11.3 Sort Almost Sorted

--- heaps/src/main/java/SortAlmostSorted.java
+++ heaps/src/main/java/SortAlmostSorted.java
@@ -1,6 +1,4 @@
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class SortAlmostSorted {
 
@@ -9,8 +7,29 @@ public class SortAlmostSorted {
     */
 
     public static List sort(Iterator<Integer> sequence, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
 
-        return Collections.emptyList();
+        LinkedList<Integer> sortedItems = new LinkedList<>();
+        for(int i = 0; i < k; i++) {
+            minHeap.add(sequence.next());
+        }
+
+        // extract next item, compare, and add next list item
+        while(sequence.hasNext()) {
+            int currentItem = sequence.next();
+            if(minHeap.size() > 0 && currentItem > minHeap.peek()) {
+                minHeap.add(currentItem);
+                currentItem = minHeap.poll();
+            }
+            sortedItems.add(currentItem);
+        }
+
+        // add remaining heap items, if any
+        while(minHeap.size() > 0) {
+            sortedItems.add(minHeap.poll());
+        }
+
+        return sortedItems;
     }
 
 }
