77fe8a171428626a75c395d295e5444d15e91a9d
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-13 17:22:36 -0800

    11.4 Compute streming median

--- heaps/src/main/java/ComputeMedianOnline.java
+++ heaps/src/main/java/ComputeMedianOnline.java
@@ -1,6 +1,4 @@
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ComputeMedianOnline {
 
@@ -9,7 +7,40 @@ public class ComputeMedianOnline {
     */
 
     public static List<Double> onlineMedian(Iterator<Integer> sequence) {
+        LinkedList<Double> medians = new LinkedList<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer x, Integer y) {
+                return y - x;
+            }
+        });
 
-        return Collections.emptyList();
+        while(sequence.hasNext()) {
+            int value = sequence.next();
+            int secondValue;
+
+            if(minHeap.size() == maxHeap.size()) {
+                if(minHeap.size() > 0) {
+                    secondValue = minHeap.poll();
+                    maxHeap.add(Math.min(value, secondValue));
+                    minHeap.add(Math.max(value, secondValue));
+                } else {
+                    maxHeap.add(value);
+                }
+                medians.add((double) maxHeap.peek());
+            } else {
+                if(minHeap.size() < maxHeap.size()) {
+                    secondValue = maxHeap.poll();
+                } else {
+                    secondValue = minHeap.poll();
+                }
+                maxHeap.add(Math.min(value, secondValue));
+                minHeap.add(Math.max(value, secondValue));
+
+                medians.add((double) (maxHeap.peek() + minHeap.peek()) / (double) 2);
+            }
+        }
+        return medians;
     }
 }
