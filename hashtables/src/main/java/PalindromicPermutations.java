bde882a245cbd6f26662b4dca3b0c3e682d1d29b
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-18 18:47:33 -0800

    13.1 Palindrome Permutation implementation

--- hashtables/src/main/java/PalindromicPermutations.java
+++ hashtables/src/main/java/PalindromicPermutations.java
@@ -1,3 +1,5 @@
+import java.util.Hashtable;
+
 public class PalindromicPermutations {
 
     /*
@@ -6,6 +8,24 @@ public class PalindromicPermutations {
 
     public static boolean canFormPalindrome(String s) {
 
-        return false;
+        Hashtable<Character, Integer> hashtable = new Hashtable<>();
+        int countOdd = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(hashtable.containsKey(s.charAt(i))) {
+                int charCount = hashtable.get(s.charAt(i));
+                if(++charCount % 2 == 0) {
+                    countOdd--;
+                } else {
+                    countOdd++;
+                }
+                hashtable.put(s.charAt(i), charCount);
+            } else {
+                hashtable.put(s.charAt(i), 1);
+                countOdd++;
+            }
+        }
+
+        return countOdd < 2;
     }
 }
