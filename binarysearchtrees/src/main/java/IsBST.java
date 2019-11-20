10b30aa5b4784c79ea22ebf456c6c8cf1f59045e
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-19 21:48:01 -0800

    15.1 Verify if BST implementation (code could be much cleaner but it works)

--- binarysearchtrees/src/main/java/IsBST.java
+++ binarysearchtrees/src/main/java/IsBST.java
@@ -3,9 +3,53 @@ public class IsBST {
     /*
     15.1
     */
+    private static class MinMax {
+        int min;
+        int max;
+    }
 
     public static boolean isBST(BinaryTree<Integer> tree) {
+        return isBSTHelper(tree) != null;
+    }
 
-        return false;
+    private static MinMax isBSTHelper(BinaryTree<Integer> node) {
+        //System.out.println(node);
+        MinMax minMax = new MinMax();
+        MinMax leftMinMax;
+        MinMax rightMinMax;
+        // base case node has no children
+        if(node.left == null && node.right == null) {
+            minMax.max = node.data;
+            minMax.min = node.data;
+        } else if (node.right == null) {
+            // node has no right children
+            leftMinMax = isBSTHelper(node.left);
+            if(leftMinMax != null && leftMinMax.max < node.data) {
+                minMax.max = node.data;
+                minMax.min = leftMinMax.min;
+            } else {
+                minMax = null;
+            }
+        } else if (node.left == null) {
+            // node has no left children
+            rightMinMax = isBSTHelper(node.right);
+            if(rightMinMax != null && rightMinMax.min > node.data) {
+                minMax.min = node.data;
+                minMax.max = rightMinMax.max;
+            } else {
+                minMax = null;
+            }
+        } else {
+            // node has children
+            leftMinMax = isBSTHelper(node.left);
+            rightMinMax = isBSTHelper(node.right);
+            if(leftMinMax != null && leftMinMax.max < node.data && rightMinMax != null && rightMinMax.min > node.data) {
+                minMax.min = leftMinMax.min;
+                minMax.min = rightMinMax.max;
+            } else {
+                minMax = null;
+            }
+        }
+        return minMax;
     }
 }
