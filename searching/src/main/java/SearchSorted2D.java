6c5fb986be202dbaff58ba85c2642d4241dbdbff
lisamke91 <lisa.m.ke91@gmail.com>
2019-11-24 21:41:56 -0800

    12.6 Search sorted 2d

--- searching/src/main/java/SearchSorted2D.java
+++ searching/src/main/java/SearchSorted2D.java
@@ -7,7 +7,27 @@ public class SearchSorted2D {
     */
 
     public static boolean search(List<List<Integer>> matrix, int x) {
+        int [][] arr = new int[matrix.size()][matrix.get(0).size()];
 
+        int index = 0;
+        for(List<Integer> row : matrix) {
+            for(int i = 0; i < row.size(); i++) {
+                arr[index][i] = row.get(i);
+            }
+            index++;
+        }
+
+        int row = 0, col = arr[0].length - 1;
+
+        while(row < arr.length && col >= 0) {
+            if(arr[row][col] == x) {
+                return true;
+            } else if(arr[row][col] < x) {
+                row++;
+            } else {
+                col--;
+            }
+        }
         return false;
     }
 }
